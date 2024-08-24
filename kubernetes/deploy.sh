226796e52bb8a9217c8a0db0f6d60f6e463c5a85
chento chea <cheachento007@gmail.com>
2024-08-24 11:18:46 +0700

    ddd

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cat << EOF > /opt/register-deploy.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+ name: register-deploy
+spec:
+ replicas: 3
+ selector:
+  matchLabels:
+   app: register-app
+ template:
+  metadata:
+   labels:
+    app: register-app
+  spec:
+   containers:
+    - name: register-cont
+      image: chentochea/kube-register:v1
+      ports:
+        - containerPort: 8080
+---
+apiVersion: v1
+kind: Service
+metadata:
+ name: register-svc
+spec:
+ type: NodePort
+ selector:
+  app: register-app
+ ports:
+  - port: 8080
+    targetPort: 8080
+    nodePort: 30003
+EOF
+
+
+kubectl apply -f /opt/register-deploy.yaml
